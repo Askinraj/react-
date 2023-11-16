792385f25602189152c0e3bebec41ea905c60ef5
Askinraj <askinraj.com>
2023-11-16 17:56:54 +0530

    added useState

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@
 import { Route,Routes,Link} from "react-router-dom";
 import Home from "./Home";
 import Post from "./Post";
+import { useState } from "react";
 function App() {
+  const [name,setName]=useState('');
+  const names=['clinton','jerome','churchil'];
+  var num = Math.floor(Math.random()*3);
+  const namesChange = ()=>{
+    setName(names[num]);
+  }
   return (
     <div >
 <Routes>
@@ -12,6 +19,7 @@ function App() {
   <p> <Link to="/home">Click Me</Link></p>
   <p><Link to="/post/1">Post one to view</Link></p>
   <p><Link to="/post/2">Post two to view</Link></p>
+  <p onClick={namesChange}>hi every on my name is {name}</p>
     </div>
   );
 }
